15cee621af7b3169261cce3aea1ca0b486a9a430
codewitharyansh07 <codewitharyansh07@gmail.com>
2024-10-31 12:41:48 +0530

    Hero Section

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Navbar from './sections/navbar';
+import Hero from './sections/Hero';
+
+const App = () => {
+    return (
+        <main className='max-w-7xl mx-auto'>
+            <Navbar />
+            <Hero />
+        </main>
+    )
+}
+
+export default App
